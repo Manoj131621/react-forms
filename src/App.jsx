91676347c6440a555345dd5d07c17e38c62317b1
Manojkumar <manojprfp@gmail.com>
2025-12-17 13:59:58 +0530

    Use fetch API for users

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Form from './components/Form'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Form from "./components/Form";
+import Users from "./components/Users";
 
 function App() {
-
   return (
-    <>
-     <Form/>
-    </>
-  )
+    <div className="container">
+      <h2>React core Practice</h2>
+      <Form />
+      <hr />
+      <Users />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+
 function Form() {
+    const [email, setEmail] = useState("")
+    const [error,setError] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(!email.includes("@")){
+            setError("Invalid email");
+            return;
+        }
+        setError("")
+        alert("Form submitted")
+    }
   return(
-    <div>Hello</div>
+    <form onSubmit={handleSubmit}>
+        <input type="text" value={email}
+        onChange={(e)=>setEmail(e.target.value)}
+        placeholder="Enter Email"/>
+        {error && <p style={{color:"red"}}>{error}</p>}
+        <button type="submit">Submit</button>
+    </form>
   )
 }
 export default Form;
